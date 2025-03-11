5bb29628addb49cff217ba53c02a7190fd737866
porotkin <preload737773@gmail.com>
2025-03-11 16:51:02 +0200

    Type-safe `SimpleTable`

--- tanstack-react-table/src/jsMain/kotlin/wrappers/example/table/PhotoTable.kt
+++ tanstack-react-table/src/jsMain/kotlin/wrappers/example/table/PhotoTable.kt
@@ -1,18 +1,17 @@
 package wrappers.example.table
 
 import react.FC
-import wrappers.example.entities.Photo
 import wrappers.example.table.selection.SelectionContext
 import wrappers.example.table.selection.selection
 import wrappers.example.table.simple.SimpleTable
-import wrappers.example.table.simple.SimpleTableProps
 
 internal val PhotoTable = FC {
     val table = usePhotoTable()
 
     SelectionContext(table.meta.selection) {
-        SimpleTable<SimpleTableProps<Photo>> {
-            value = table
+        SimpleTable {
+            headerGroups = table.headerGroups
+            rows = table.rows
         }
     }
 }
--- tanstack-react-table/src/jsMain/kotlin/wrappers/example/table/simple/SimpleTable.kt
+++ tanstack-react-table/src/jsMain/kotlin/wrappers/example/table/simple/SimpleTable.kt
@@ -1,10 +1,14 @@
 package wrappers.example.table.simple
 
 import emotion.styled.styled
+import js.array.ReadonlyArray
 import react.ChildrenBuilder
 import react.FC
-import react.PropsWithValue
+import react.Props
+import react.ReactDsl
 import react.dom.html.ReactHTML.table
+import tanstack.table.core.HeaderGroup
+import tanstack.table.core.Row
 import web.cssom.Auto.Companion.auto
 import web.cssom.Border
 import web.cssom.BorderCollapse
@@ -13,22 +17,27 @@ import web.cssom.WhiteSpace
 import web.cssom.px
 import wrappers.example.theme.Theme
 
-internal external interface SimpleTableProps<D : Any> : PropsWithValue<TableInstance<D>>
+internal external interface SimpleTableProps<D : Any> : Props {
+    var headerGroups: ReadonlyArray<HeaderGroup<D>>
+    var rows: ReadonlyArray<Row<D>>
+}
 
-internal val SimpleTable: FC<SimpleTableProps<*>> = FC { props ->
+internal fun <D : Any> ChildrenBuilder.SimpleTable(block: @ReactDsl SimpleTableProps<D>.() -> Unit) {
+    SimpleTable.invoke(block)
+}
+
+private val SimpleTable: FC<SimpleTableProps<*>> = FC { props ->
     SimpleTable(props)
 }
 
 private fun <D : Any> ChildrenBuilder.SimpleTable(props: SimpleTableProps<D>) {
-    val table = props.value
-
     TableBase {
         TableHead {
-            value = table.headerGroups
+            value = props.headerGroups
         }
 
         TableBody {
-            value = table.rows
+            value = props.rows
         }
     }
 }
