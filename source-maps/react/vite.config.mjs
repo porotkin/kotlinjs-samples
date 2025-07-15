b1dd90af9b2bdbe4762f14d645e1b8b24b949c82
porotkin <preload737773@gmail.com>
2025-07-15 20:14:27 +0300

    Source Maps. `react` Fix compilation without envs

--- source-maps/react/vite.config.mjs
+++ source-maps/react/vite.config.mjs
@@ -4,15 +4,17 @@ import sourcemaps from 'rollup-plugin-sourcemaps'
 
 export default defineConfig(({mode}) => {
     const env = loadEnv(mode, process.cwd(), '')
+    const sentryToken = env.VITE_SENTRY_AUTH_TOKEN
 
     return {
         root: "kotlin",
         plugins: [
             sourcemaps(),
             sentryVitePlugin({
+                disable: !sentryToken,
+                authToken: sentryToken,
                 org: env.VITE_SENTRY_ORG,
                 project: env.VITE_SENTRY_PROJECT,
-                authToken: env.VITE_SENTRY_AUTH_TOKEN,
             }),
         ],
     }
