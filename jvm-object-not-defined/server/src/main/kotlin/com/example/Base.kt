53386dcdb8aad65f924b9fd32deeb3fedc974204
porotkin <preload737773@gmail.com>
2025-07-17 13:14:14 +0300

    `jvm-object-not-defined` Base. Remove lazy

--- jvm-object-not-defined/server/src/main/kotlin/com/example/Base.kt
+++ jvm-object-not-defined/server/src/main/kotlin/com/example/Base.kt
@@ -8,22 +8,19 @@ interface Base {
 
     companion object {
         // order is important for sorting by priority
-        val allFormats: List<Base> by lazy {
+        val allFormats: List<Base> =
             listOf(
                 Derived,
-            )
-        }
+            ).also { list ->
+                list.forEach {
+                    requireNotNull(it.id)
+                }
+            }
 
         private val priorities: Map<Base, Int> by lazy {
             allFormats
                 .mapIndexed { index, format -> format to -index }
                 .toMap()
-        }
-
-        init {
-            allFormats.forEach {
-                requireNotNull(it.id)
-            }
         }
     }
 }
