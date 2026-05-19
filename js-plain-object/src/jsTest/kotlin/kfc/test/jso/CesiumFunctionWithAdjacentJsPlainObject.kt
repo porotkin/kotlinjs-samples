728cbbebfb65c7f9570e806c494d1853ef0beba0
porotkin <preload737773@gmail.com>
2026-05-19 11:31:28 +0300

    Js Plain Object. Ignore cesium test

--- js-plain-object/src/jsTest/kotlin/kfc/test/jso/CesiumFunctionWithAdjacentJsPlainObject.kt
+++ js-plain-object/src/jsTest/kotlin/kfc/test/jso/CesiumFunctionWithAdjacentJsPlainObject.kt
@@ -1,10 +1,12 @@
 package kfc.test.jso
 
 import kotlinx.coroutines.test.runTest
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class CesiumFunctionWithAdjacentJsPlainObject {
+    @Ignore
     @Test
     fun browserEventExternal() = runTest {
         functionWithAdjacentJsPlainObject()
