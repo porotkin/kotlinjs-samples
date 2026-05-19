78f557adb51362e357a4aa07b38839e16db33e27
porotkin <preload737773@gmail.com>
2026-05-19 11:32:09 +0300

    Source Maps. `react` Comment out rollup sourcemaps plugin usage

--- source-maps/react/vite/vite.config.mjs
+++ source-maps/react/vite/vite.config.mjs
@@ -1,6 +1,5 @@
 import {defineConfig, loadEnv} from 'vite'
 import {sentryVitePlugin} from "@sentry/vite-plugin";
-import sourcemaps from 'rollup-plugin-sourcemaps'
 
 export default defineConfig(({mode}) => {
     const env = loadEnv(mode, process.cwd(), '')
@@ -9,7 +8,7 @@ export default defineConfig(({mode}) => {
     return {
         root: "kotlin",
         plugins: [
-            sourcemaps(),
+            // sourcemaps(),
             sentryVitePlugin({
                 disable: !sentryToken,
                 authToken: sentryToken,
