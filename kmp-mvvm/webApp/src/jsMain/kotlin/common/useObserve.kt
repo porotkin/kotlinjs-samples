5c9ad085cdfb9fa2f0450feb074a2bd8e6c9e98f
porotkin <preload737773@gmail.com>
2026-08-25 16:23:16 +0300

    `kmp-mvvn` Refactor `useObserve` to extract `StateFlow.subscribe` for improved readability and reuse

--- kmp-mvvm/webApp/src/jsMain/kotlin/common/useObserve.kt
+++ kmp-mvvm/webApp/src/jsMain/kotlin/common/useObserve.kt
@@ -1,7 +1,6 @@
 import com.example.kmp_mvvm.viewmodel.ObservableProperty
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -12,17 +11,17 @@ import react.useSyncExternalStore
 @Suppress("UNCHECKED_CAST")
 fun <T> useObserve(state: ObservableProperty<T>): T {
     val stateFlow = state as StateFlow<T>
-
     val subscribe = useCallback(stateFlow) { onStoreChange: () -> Unit ->
-        val scope = CoroutineScope(Dispatchers.Unconfined)
-
-        stateFlow
-            .onEach { onStoreChange() }
-            .launchIn(scope)
-
-        val cleanup: Cleanup = { scope.cancel() }
-        cleanup
+        stateFlow.subscribe(onStoreChange)
     }
-
     return useSyncExternalStore(subscribe, getSnapshot = { stateFlow.value })
+}
+
+private fun StateFlow<*>.subscribe(
+    onStoreChange: () -> Unit,
+): Cleanup {
+    val job = onEach { onStoreChange() }
+        .launchIn(CoroutineScope(Dispatchers.Unconfined))
+
+    return { job.cancel() }
 }
